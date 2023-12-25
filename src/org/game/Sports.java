b8f3eca3947aaa0a2ee3d9ec71c6fe402b00e7ba
bsaiswarya2816 <bsaiswarya2816.com>
2023-12-25 12:11:35 +0530

    Initial

---
+++ src/org/game/Sports.java
@@ -0,0 +1,12 @@
+package org.game;
+
+public class Sports {
+	
+	private void cricket() {
+		System.out.println("cricket");
+	}
+	private void snowBall() {
+		System.out.println("snowBall");
+	}
+
+}
