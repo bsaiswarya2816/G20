25e7f40cd1f9c40c02f3118c03fced714991f355
bsaiswarya2816 <bsaiswarya2816.com>
2023-12-25 12:39:19 +0530

    Process1Changed

--- src/org/game/Sports.java
+++ src/org/game/Sports.java
@@ -8,5 +8,11 @@ public class Sports {
 	private void snowBall() {
 		System.out.println("snowBall");
 	}
+	private void handBall() {
+		System.out.println("HandBall");
+	}
+	private void throwBall() {
+		System.out.println("ThrowBall");
+	}
 
 }
